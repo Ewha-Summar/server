50ebd469233035fad3b3d51d6b27afbbf3e7d412
Haneul99 <healing5746@gmail.com>
2021-02-16 11:56:30 +0900

    test

--- summarize_quiz/test.py
+++ summarize_quiz/test.py
@@ -1,19 +1,52 @@
-from flask import jsonify
+from flask import jsonify, Flask, request, session, url_for, redirect, render_template, g, flash, abort
+import json
+from werkzeug.exceptions import HTTPException, NotFound
 
-response = {}
-response['status'] = 200
-response['success'] = True
-response['score'] = str(3) + '/' + str(5)
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-data = {}
-data['correct_list'] = []
-quiz = {}
-quiz['quiz_id'] = 1
-quiz['content'] = "퀴즈1 내용"
-quiz['correct'] = True
-data['correct_list'].append(quiz)
+'''
+@app.errorhandler(HTTPException)
+def error_handler(e):
+    response = e.get_response()
 
-response['data'] = data
+    response.data = json.dumps({
+        'status': e.code,
+        'success': False,
+        'message': e.description,
+    })
+    response.content_type = 'application/json'
 
-#print(jsonify(response))
-print(response)
+    return response
+'''
+
+
+@app.route('/')
+def index():
+    print("main page")
+    return "Hello World"
+    '''
+    response = {}
+    response['status'] = 200
+    response['success'] = True
+    response['score'] = str(3) + '/' + str(5)
+
+    data = {}
+    data['correct_list'] = []
+    quiz = {}
+    quiz['quiz_id'] = 1
+    quiz['content'] = "퀴즈1 내용"
+    quiz['correct'] = True
+    data['correct_list'].append(quiz)
+
+    response['data'] = data
+
+    return print(jsonify(response))
+'''
+
+@app.route('/user')
+def user():
+    return "user"
+
+if __name__ == "__main__":
+    app.run()
