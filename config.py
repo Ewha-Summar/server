93e0a2ec94aaa1cf3258f5c71b93fade27edd600
KimJeeWoon <66114269+KimJeeWoon@users.noreply.github.com>
2021-03-08 16:25:50 +0900

    changed

--- config.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user'     : 'admin',
-    'password' : 'ewha2020summar',
-    'host'     : 'db-summar-server.cpemlh0lctvc.us-east-2.rds.amazonaws.com',
-    'port'     : 3306,
-    'database' : 'summardb'
-}
-
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
