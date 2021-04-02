83308cdf5a066e5d66d17e0961d8703996d6a1b0
KimJeeWoon <66114269+KimJeeWoon@users.noreply.github.com>
2021-04-02 15:10:11 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -586,6 +586,6 @@ def qna():
 
 
 if __name__ == "__main__":
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain(certfile='private.pem', keyfile='private.pem', password='ewha2020summar')
-    app.run(host="0.0.0.0", port="5000", ssl_context=ssl_context)
+    #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    #ssl_context.load_cert_chain(certfile='private.pem', keyfile='private.pem', password='ewha2020summar')
+    app.run(host="0.0.0.0", port="5000", ssl_context='adhoc')
