17a1e376479667d7dbe7a2c9dfa438856a6352e4
JeeWoon <66114269+KimJeeWoon@users.noreply.github.com>
2021-06-11 11:16:44 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -107,7 +107,7 @@ def login():
             'user_id' : user_id
         }
         token = jwt.encode(payload, SECRET_KEY, ALGORITHM)#토큰생성
-        token.decode('utf-8')
+        token.encode('utf-8')
         print(token)
 
         return jsonify({
